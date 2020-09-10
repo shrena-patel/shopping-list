6ff2b22f7aa1d8006412fdce7d0cdb5b6351a04b
cameron <cameronshaw95@gmail.com>
2020-09-11 10:36:32 +1200

    remove old comments

--- client/components/Pic.jsx
+++ client/components/Pic.jsx
@@ -1,22 +1,5 @@
 import React from "react"
 
-// const Pic = () => {
-//     return (
-//         <>
-//         <article className="tile is-child notification is-info">
-//           <div className="content">
-//               <p className="title">Pic of the day</p>
-//               <div className="content">
-//                   <p>-</p>
-//                   <p>-</p>
-//                   <p>-</p>
-//               </div>
-//           </div>
-//         </article>
-//         </>
-//     )
-// }
-
 class Pic extends React.Component {
   componentDidMount() {}
 
