2b9b466dc0360ecba75b12cb6d6bcd24f57bc9ca
cameron <cameronshaw95@gmail.com>
2020-09-11 10:49:00 +1200

    add state

--- client/components/Pic.jsx
+++ client/components/Pic.jsx
@@ -1,13 +1,31 @@
 import React from "react"
-
+import { getNasaPhoto } from "../apis/image"
 class Pic extends React.Component {
-  componentDidMount() {}
+  state = {
+    nasaPhotoInfo: {},
+  }
+
+  componentDidMount() {
+    this.refreshNasaPhoto()
+  }
+
+  refreshNasaPhoto() {
+    getNasaPhoto().then((nasaPhotoInfo) => {
+      this.setState({
+        nasaPhotoInfo: nasaPhotoInfo,
+      })
+    })
+  }
 
   render() {
+    const nasaPic = this.state.nasaPhotoInfo
     return (
-      <div className="image-container tile is-child notification is-info">
-        <img src={this.props.url} className="img" alt={this.props.title} />
-      </div>
+      <>
+        <div className="image-container tile is-child notification is-info">
+          <p className="title">Pic of the day</p>
+          <img src={nasaPic.url} className="img" alt={nasaPic.title} />
+        </div>
+      </>
     )
   }
 }
