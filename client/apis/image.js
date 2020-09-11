7198b7041515dbc6bfadf6a6b121fdb55e4a29b9
cameron <cameronshaw95@gmail.com>
2020-09-11 13:38:16 +1200

    fix api issue

--- client/apis/image.js
+++ client/apis/image.js
@@ -3,21 +3,24 @@ import request from "superagent"
 const apiKey = "dGXdFjaqtDknxoN3eThl13Ac6IamoZegMJIfkbBM"
 
 const placeholder = {
-  image: [
-    {
-      url: "https://miro.medium.com/max/1600/1*zRvYJWeLF5dcxM1G2hcEHA.jpeg",
-    },
-  ],
+  url: "https://miro.medium.com/max/1600/1*zRvYJWeLF5dcxM1G2hcEHA.jpeg",
 }
 
 export function getNasaPhoto() {
   return request
-    .get(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`)
+    .get(
+      `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=2020-09-09`
+    )
     .then((response) => {
-      if (response.body.length !== 0) {
+      if (response.body.url) {
+        console.log(response.headers)
         return response.body
       } else {
         return placeholder
       }
+    })
+    .catch((err) => {
+      console.log(err)
+      return placeholder
     })
 }
